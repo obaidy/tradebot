1452032e6a7aedecaa006d9d44b1859771bdf381
obaidy <ahmed.obaidy12@gmail.com>
2025-09-30 01:16:47 +0300

    fav

--- apps/portal/pages/_app.tsx
+++ apps/portal/pages/_app.tsx
@@ -9,6 +9,8 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps<{ ses
   return (
     <SessionProvider session={session}>
       <Head>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/favicon.png" />
       </Head>
