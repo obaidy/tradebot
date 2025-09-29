ca11660cbac97a82bdb08caf4725ab5d8b0c9bb4
obaidy <ahmed.obaidy12@gmail.com>
2025-09-30 01:10:53 +0300

    favicon

--- apps/portal/pages/_app.tsx
+++ apps/portal/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import type { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
@@ -7,6 +8,10 @@ import { OctopusLoader } from '../components/ui/OctopusLoader';
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session | null }>) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
       <OctopusLoader />
     </SessionProvider>
