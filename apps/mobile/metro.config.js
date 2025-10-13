59c1474f323e697815bd821d37cbaf3c45d914c3
obaidy <ahmed.obaidy12@gmail.com>
2025-10-13 20:38:50 +0300

    metro

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,5 +1,13 @@
+const path = require('path');
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const config = getDefaultConfig(projectRoot);
+
+config.resolver = config.resolver || {};
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  'react-native': path.resolve(projectRoot, 'node_modules/react-native'),
+};
 
 module.exports = config;
